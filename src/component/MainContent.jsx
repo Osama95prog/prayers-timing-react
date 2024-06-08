93bcd66da2bc12c5d041fb089f43e004ae692f64
Osama ALBaida <osama9boo@gmail.com>
2024-06-08 15:42:04 +0300

    building praying cards in fetching data from api

--- src/component/MainContent.jsx
+++ src/component/MainContent.jsx
@@ -1,103 +1,181 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Prayer from "./Prayer";
 import Stack from "@mui/material/Stack";
-import { Divider, Grid } from "@mui/material";
+import { Divider } from "@mui/material";
+import Grid from "@mui/material/Grid"; // Grid version 1
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
-
-
+import axios from "axios";
+import moment from "moment";
 
 export default function MainContent() {
+	//all my  state
+  const [timings, setTimings] = useState({
+    Fajr: "04:20",
+    Dhuhr: "11:50",
+    Asr: "15:18",
+    Sunset: "18:03",
+    Isha: "19:33",
+  });
+  const [selectedCity, setSelectedCity] = useState({
+    apiName: "Damascus",
+    displayName: "دمشق",
+  });
 
-  
+  const [today, setToday] = useState("")
+
+//   End state 
+
+  const avilableCities = [
+    {
+      apiName: "Damascus",
+      displayName: "دمشق",
+    },
+    {
+      apiName: "Aleepo",
+      displayName: "حلب",
+    },
+    {
+      apiName: "Homs",
+      displayName: "حمص",
+    },
+    {
+      apiName: "Lataqia",
+      displayName: "اللاذقية",
+    },
+    {
+      apiName: "Hama",
+      displayName: "حماة",
+    },
+  ];
+
+  const handleCityChange = (event) => {
+    console.log(event);
+    const cityObject = avilableCities.find((city) => {
+      return city.apiName == event.target.value;
+    });
+    console.log("the new value is ", event.target.value);
+    setSelectedCity(cityObject);
+  };
+
+  const timingsApi = async () => {
+    const response = await axios.get(
+      `https://api.aladhan.com/v1/timingsByCity?city=${selectedCity.apiName}&country=Syria`
+    );
+    console.log({ response });
+    setTimings(response.data.data.timings);
+  };
+
+  useEffect(() => {
+	console.log('calling the api');
+    timingsApi();
+  },[selectedCity]);
+
+  useEffect(() => {
+	const t = moment();
+	setToday(t.format("MMMM Do YYYY | hh:mm:ss a"))
+  })
+
   return (
-    <div style={{ color: '#fff' }}>
-        <Grid container style={{ background : "#bbb", paddingRight: "12px",borderRadius: "10px" }} >
-            <Grid xs={6}>
-                <div>
-                    <h2>سبتمبر 9 2023 | 4:20</h2>
-                    <h1>مكة المكرمة</h1>
-                </div>
-            </Grid>
-            <Grid xs={6}>
-               <div>
-                <h2>الوقت المتبقي لصلاة العصر</h2>
-                <h1> 7 : 25 : 30</h1>
-               </div>
-            </Grid>
+    <div style={{ color: "#999" }}>
+      <Grid
+        container
+        style={{
+          background: "#fff",
+          paddingRight: "30px",
+          borderRadius: "10px",
+        }}
+      >
+        <Grid md={6}>
+          <div>
+            <h2>سبتمبر 9 2023 | 4:20</h2>
+            <h2>{today}</h2>
+            <h1>
+				{selectedCity.displayName}
+			</h1>
+          </div>
         </Grid>
-       {/* TOP ROW  */}
+        <Grid md={6}>
+          <div>
+            <h2>الوقت المتبقي لصلاة العصر</h2>
+            <h1> 7 : 25 : 30</h1>
+          </div>
+        </Grid>
+      </Grid>
+      {/* TOP ROW  */}
 
-			<Divider style={{ borderColor: "#777", opacity: "0.5", marginTop: "10px"  }} />
+      <Divider
+        style={{ borderColor: "#fff", opacity: "0.5", marginTop: "20px" }}
+      />
 
-    {/* PRAYERS CARDS */}
-			<Stack
-				direction="row"
-				justifyContent={"space-around"}
-				style={{ marginTop: "50px" }}
-			>
-				<Prayer
-					name="الفجر"
-					// time={timings.Fajr}
-					image="https://wepik.com/api/image/ai/9a07baa7-b49b-4f6b-99fb-2d2b908800c2"
-				/>
-				<Prayer
-					name="الظهر"
-					// time={timings.Dhuhr}
-					image="https://wepik.com/api/image/ai/9a07bb45-6a42-4145-b6aa-2470408a2921"
-				/>
-				<Prayer
-					name="العصر"
-					// time={timings.Asr}
-					image="https://wepik.com/api/image/ai/9a07bb90-1edc-410f-a29a-d260a7751acf"
-				/>
-				<Prayer
-					name="المغرب"
-					// time={timings.Sunset}
-					image="https://wepik.com/api/image/ai/9a07bbe3-4dd1-43b4-942e-1b2597d4e1b5"
-				/>
-				<Prayer
-					name="العشاء"
-					// time={timings.Isha}
-					image="https://wepik.com/api/image/ai/9a07bc25-1200-4873-8743-1c370e9eff4d"
-				/>
-			</Stack>
-			{/*== PRAYERS CARDS ==*/}
+      {/* PRAYERS CARDS */}
+      <Stack
+        direction="row"
+        justifyContent={"space-around"}
+        style={{ marginTop: "30px" }}
+      >
+        <Prayer
+          name="الفجر"
+          time={timings.Fajr}
+          image="https://wepik.com/api/image/ai/9a07baa7-b49b-4f6b-99fb-2d2b908800c2"
+        />
+        <Prayer
+          name="الظهر"
+          time={timings.Dhuhr}
+          image="https://wepik.com/api/image/ai/9a07bb45-6a42-4145-b6aa-2470408a2921"
+        />
+        <Prayer
+          name="العصر"
+          time={timings.Asr}
+          image="https://wepik.com/api/image/ai/9a07bb90-1edc-410f-a29a-d260a7751acf"
+        />
+        <Prayer
+          name="المغرب"
+          time={timings.Sunset}
+          image="https://wepik.com/api/image/ai/9a07bbe3-4dd1-43b4-942e-1b2597d4e1b5"
+        />
+        <Prayer
+          name="العشاء"
+          time={timings.Isha}
+          image="https://wepik.com/api/image/ai/9a07bc25-1200-4873-8743-1c370e9eff4d"
+        />
+      </Stack>
+      {/*== PRAYERS CARDS ==*/}
 
       {/* SELECT CITY */}
-			<Stack
-				direction="row"
-				justifyContent={"center"}
-				style={{ marginTop: "40px" }}
-			>
-				<FormControl style={{ width: "20%" }}>
-					<InputLabel id="demo-simple-select-label">
-						<span style={{ color: "#333" }}>المدينة</span>
-					</InputLabel>
-					<Select
-						style={{ color: "#333" }}
-						labelId="demo-simple-select-label"
-						id="demo-simple-select"
-						// value={age}
-						label="Age"
-						// onChange={handleCityChange}
-					>
-						{/* {avilableCities.map((city) => {
-							return (
-								<MenuItem
-									value={city.apiName}
-									key={city.apiName}
-								>
-									{city.displayName}
-								</MenuItem>
-							);
-						})} */}
-					</Select>
-				</FormControl>
-			</Stack>
- 
+      <Stack
+        direction="row"
+        justifyContent={"center"}
+        style={{ marginTop: "40px", marginBottom: "40px" }}
+      >
+        <FormControl style={{ width: "20%" }}>
+          <InputLabel id="demo-simple-select-label">
+            <span style={{ color: "#fff" }}>المدينة</span>
+          </InputLabel>
+          <Select
+            style={{ color: "#fff" }}
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            // value={age}
+            label="City"
+            onChange={handleCityChange}
+          >
+            {avilableCities.map((city) => {
+              return (
+                <MenuItem value={city.apiName} key={city.apiName}>
+                  {city.displayName}
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
+      </Stack>
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
-  )
+  );
 }
--- src/component/Prayer.jsx
+++ src/component/Prayer.jsx
@@ -1,3 +1,4 @@
+import { Divider } from "@mui/material";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
@@ -5,19 +6,30 @@ import Typography from "@mui/material/Typography";
 
 export default function Prayer({ name, time, image }) {
 	return (
-		<Card sx={{ width: "14vw" }}>
+		<Card sx={{ width: "13vw",padding: '11px' }}>
 			<CardMedia
-				sx={{ height: 140 }}
+				sx={{ height: 100 }}
 				image={image}
 				title="green iguana"
 			/>
+				<Divider
+					style={{ borderColor: "#000", opacity: "0.8", marginTop: "8px" }}
+				/>
 			<CardContent>
-				<h2>{name}</h2>
+				<h2 style={{ marginBottom: '7px' }}>{name}</h2>
+				<Divider
+					style={{ borderColor: "#000", opacity: "0.8", marginBottom: "30px" }}
+				/>
 
-				<Typography variant="h1" color="text.secondary">
+				
+				<Typography variant="h3" color="text.secondary">
 					{time}
 				</Typography>
 			</CardContent>
+				{/* <Divider
+					style={{ borderColor: "#000", opacity: "0.8", marginBottom: "3px" }}
+				/> */}
 		</Card>
+		
 	);
 }
